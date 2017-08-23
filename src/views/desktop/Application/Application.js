551adc3850c6d6ac8282ae89d8439f6a790342cd
Mathis <mathis.biabiany@gmail.com>
2017-08-24 00:28:39 +0200

    Setup

--- src/views/desktop/Application/Application.js
+++ src/views/desktop/Application/Application.js
@@ -1,4 +1,5 @@
 import * as pages from 'core/pages';
+import LoaderView from 'views/common/Loader/Loader';
 import HomeView from 'views/desktop/Home/Home';
 
 export default class DesktopAppView {
@@ -8,11 +9,22 @@ export default class DesktopAppView {
   constructor() {
     this.el = document.getElementById('application');
 
+    this.views = [];
+    this.loader = this.setupLoader();
     this.home = this.setupHome();
+
+    this.views.push(this.loader, this.home);
+  }
+
+  setupLoader() {
+    const view = new LoaderView({
+      parent: this.el,
+    });
+
+    return view;
   }
 
   setupHome() {
-
     const view = new HomeView({
       parent: this.el,
     });
