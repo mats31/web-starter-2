3be534ddfd3fa08137da641725fe71d5d8cc86e2
Mathis Biabiany <mathis.biabiany@gmail.com>
2019-03-06 01:36:02 +0100

    Update utils

--- src/utils/canvas/index.js
+++ src/utils/canvas/index.js
@@ -1,5 +1,6 @@
-import createCanvas from './createCanvas';
-import createHexagone from './createHexagone';
-import resizeCanvas from './resizeCanvas';
+import createCanvas from './createCanvas'
+import createHexagone from './createHexagone'
+import resizeCanvas from './resizeCanvas'
+import roundRect from './roundRect'
 
-export { createCanvas, createHexagone, resizeCanvas };
+export { createCanvas, createHexagone, resizeCanvas, roundRect }
