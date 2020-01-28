b3d1abf34ba6095c9f0098df0bf13c14ffe59ee2
Mathis Biabiany <mathis.biabiany@gmail.com>
2020-01-28 12:17:20 +0900

    Update modules

--- webpack/webpack.prod.config.babel.js
+++ webpack/webpack.prod.config.babel.js
@@ -6,7 +6,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 const Config = {
   mode: 'production',
@@ -16,7 +16,7 @@ const Config = {
   },
   output: {
     path: path.join(__dirname, '..', 'build'),
-    filename: '[name]-[hash].min.js',
+    filename: '[name].min.js',
   },
   resolve: {
     modules: [
@@ -126,14 +126,15 @@ const Config = {
     new MiniCssExtractPlugin({
       // Options similar to the same options in webpackOptions.output
       // both options are optional
-      filename: '[name]-[hash].min.css',
+      filename: '[name].min.css',
       chunkFilename: "[id].css"
     }),
     new CleanWebpackPlugin(['build'], { root: path.resolve(__dirname, '..') }),
   ],
   optimization: {
+    minimize: true,
     minimizer: [
-      new UglifyJsPlugin({
+      new TerserPlugin({
         cache: true,
         parallel: true,
         sourceMap: true // set to true if you want JS source maps
