73996a28fdbd75fcda611f86081c9b1861ecd066
Mathis <mathis.biabiany@gmail.com>
2021-01-21 17:06:25 +0100

    Updation postion gui

--- src/utils/3d/addPositionGUI.js
+++ src/utils/3d/addPositionGUI.js
@@ -1,12 +1,32 @@
-export default function(GUI, GUIObject, object, min = -50, max = 50) {
-  const folder = GUI.addFolder(`Position - ${object.id}`)
+export default function(GUI, GUIObject, object, min = -50, max = 50, onComplete = () => {} ) {
+  const folder = GUI.addFolder(`Matrix - ${object.id}`)
+
   GUIObject[object.id] = {
-    x: 0,
-    y: 0,
-    z: 0,
+    x: object.position.x,
+    y: object.position.y,
+    z: object.position.z,
+    rotX: object.rotation.x,
+    rotY: object.rotation.y,
+    rotZ: object.rotation.z,
   }
 
-  folder.add(GUIObject[object.id], 'x', min, max).onChange((value) => { object.position.x = value })
-  folder.add(GUIObject[object.id], 'y', min, max).onChange((value) => { object.position.y = value })
-  folder.add(GUIObject[object.id], 'z', min, max).onChange((value) => { object.position.z = value })
+  folder.add(GUIObject[object.id], 'x', min, max).onChange((value) => { object.position.x = value
+  onComplete()
+  })
+  folder.add(GUIObject[object.id], 'y', min, max).onChange((value) => { object.position.y = value
+  onComplete()
+  })
+  folder.add(GUIObject[object.id], 'z', min, max).onChange((value) => { object.position.z = value
+  onComplete()
+  })
+
+  folder.add(GUIObject[object.id], 'rotX', min, max).onChange((value) => { object.rotation.x = value
+  onComplete()
+  })
+  folder.add(GUIObject[object.id], 'rotY', min, max).onChange((value) => { object.rotation.y = value
+  onComplete()
+  })
+  folder.add(GUIObject[object.id], 'rotZ', min, max).onChange((value) => { object.rotation.z = value
+  onComplete()
+  })
 }
